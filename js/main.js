d461924cc98ebb8e577f9ad03f6a70ed768e16ed
sashankneupane7 <sashankneupane7@gmail.com>
2021-03-10 09:58:51 +0545

    added timer and sorted videos (audio and custom timer still not working)

---
+++ js/main.js
@@ -0,0 +1,114 @@
+var mainVideo = document.getElementById("rain-vid");
+var timer = 0;
+
+function checkPlay(video) {
+  isPlaying = document.getElementById("ppButton").src.endsWith("pause.png");
+  if (isPlaying) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function fire() {
+  video = document.getElementById("fire-vid");
+  mainVideo = video;
+  checkPlay(video);
+  document.getElementById("fire-vid").style.display = "initial";
+  document.getElementById("beach-vid").style.display = "none";
+  document.getElementById("rain-vid").style.display = "none";
+  document.getElementById("wind-vid").style.display = "none";
+  return video;
+}
+
+function beach() {
+  video = document.getElementById("beach-vid");
+  mainVideo = video;
+  checkPlay(video);
+  document.getElementById("beach-vid").style.display = "initial";
+  document.getElementById("fire-vid").style.display = "none";
+  document.getElementById("rain-vid").style.display = "none";
+  document.getElementById("wind-vid").style.display = "none";
+  return video;
+}
+
+function wind() {
+  video = document.getElementById("wind-vid");
+  mainVideo = video;
+  checkPlay(video);
+  document.getElementById("wind-vid").style.display = "initial";
+  document.getElementById("beach-vid").style.display = "none";
+  document.getElementById("rain-vid").style.display = "none";
+  document.getElementById("fire-vid").style.display = "none";
+  return video;
+}
+
+function rain() {
+  video = document.getElementById("rain-vid");
+  mainVideo = video;
+  checkPlay(video);
+  document.getElementById("rain-vid").style.display = "initial";
+  document.getElementById("beach-vid").style.display = "none";
+  document.getElementById("fire-vid").style.display = "none";
+  document.getElementById("wind-vid").style.display = "none";
+  return video;
+}
+
+
+
+function playPause() {
+  ppButton = document.getElementById("ppButton").src;
+  if (ppButton.endsWith("pause.png")) {
+    console.log(mainVideo)
+    ppButton = ppButton.replace("pause.png", "play.png");
+    document.getElementById("ppButton").src = ppButton;
+    mainVideo.pause();
+  } else {
+    ppButton = ppButton.replace("play.png", "pause.png");
+    document.getElementById("ppButton").src = ppButton;
+    mainVideo.play();
+  }
+}
+
+function twoMinutes(){
+  timer = 120000;
+  timerDisplay.innerHTML = msToTimeString(timer);
+}
+
+function fiveMinutes(){
+  timer = 300000;
+  timerDisplay.innerHTML = msToTimeString(timer);
+}
+
+function tenMinutes(){
+  timer = 600000;
+  timerDisplay.innerHTML = msToTimeString(timer);
+}
+
+function msToTimeString(ms) {
+  let seconds = (ms / 1000) % 60;
+  let minutes = Math.floor(ms / 1000 / 60) % 60;
+  let hours = Math.floor(ms / 1000 / 60 / 60);
+
+  seconds = ("0" + seconds).slice(-2);
+  minutes = ("0" + minutes).slice(-2);
+  hours = ("0" + hours).slice(-2);
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+const timerDisplay = document.getElementById("timer-display");
+const timerButton = document.getElementById("ppButton");
+
+timerButton.addEventListener("click", () => {
+  setInterval(() => {
+    timer -= 1000;
+    if (timer<0) {
+      mainVideo.pause();
+      document.getElementById("ppButton").src = document.getElementById('ppButton').src.replace("pause.png","play.png");
+      return;
+    }
+    timerDisplay.innerHTML = msToTimeString(timer);
+  }, 1000);
+});
+
